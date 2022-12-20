cf074c5f2a2f1eb8f30f47d36072cc6a396f59a9
Ricardo Araújo <ricardoaraujo@Ricardos-Air.home>
2022-12-20 05:31:40 +0000

    Web começado

---
+++ FeirasNovas/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FeirasNovas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace FeirasNovas.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Product> Produto { get; set; }
+}
+
---
+++ FeirasNovas/Models/Sales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FeirasNovas.Models
+{
+    public class Sales
+    {
+        [Key]
+        public int idVenda { get; set; }
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+        public string Fatura { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
+
