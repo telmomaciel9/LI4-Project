cfa9a2a71ab4034b50af084aa21b9a923e6cddbe
Telmo Maciel <telmomaciel9@gmail.com>
2023-01-15 03:46:21 +0000

    sdfg

--- FeirasNovas/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FeirasNovas.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
--- README.md
+++
@@ -1,2 +0,0 @@
-# FeirasNovas
-LI4 project
