9fbe76ba3622dcd2fa78af9f1d3f2b3aa407d2a6
Telmo Maciel <telmomaciel9@gmail.com>
2023-01-15 03:39:28 +0000

    restore

--- FeirasNovas/Data/ApplicationDbContext.cs
+++ FeirasNovas/Data/ApplicationDbContext.cs
@@ -15,35 +15,5 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Sales> Venda { get; set; }
     public DbSet<Feiras> Feiras { get; set; }
 
-<<<<<<< HEAD
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        builder.Entity<Feira_Product>().HasKey(am => new
-        {
-            am.idFeira,
-            am.idProduto
-        });
-
-        builder.Entity<Feira_Product>().HasOne(m => m.Feiras).WithMany(am => am.Feira_Products).HasForeignKey(m => m.idFeira);
-        builder.Entity<Feira_Product>().HasOne(m => m.Product).WithMany(am => am.Feira_Products).HasForeignKey(m => m.idProduto);
-
-        base.OnModelCreating(builder);
-
-
-        //builder.Entity<Feiras>()
-        //    .HasMany(f => f.Vendedores)
-        //    .WithOne()
-        //    .OnDelete(DeleteBehavior.Cascade);
-    }
-     
-    public DbSet<Feiras> Feira { get; set; }
-
-    public DbSet<Product> Products { get; set; }
-
-    public DbSet<Feira_Product> Feira_Products { get; set; }
-
-
-=======
->>>>>>> b3f80d55761f5e7dac1a2a5d267adb0f19c70dc4
 }
 
