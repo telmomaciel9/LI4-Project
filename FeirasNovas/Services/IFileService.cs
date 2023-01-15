8bfc028033a3fd2a2f2d31036791c56fdbf9f939
Telmo Maciel <telmomaciel9@gmail.com>
2023-01-15 03:58:12 +0000

    sdfg

--- FeirasNovas/Services/IFileService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FeirasNovas.Services
-{
-    public interface IFileService
-    {
-        Tuple<int, string> SaveImage(IFormFile imageFile);
-        public bool DeleteImage(string imageFileName);
-    }
-}
